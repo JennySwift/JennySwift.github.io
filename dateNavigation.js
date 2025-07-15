5cc7ba26458f5a104db1b7fe482370405aff304c
Jenny Swift <jennyswiftcreations@gmail.com>
2025-07-16 09:50:20 +1000

    Experiment

--- dateNavigation.js
+++ dateNavigation.js
@@ -20,3 +20,31 @@ document.getElementById("nextDate").addEventListener("click", () => {
     selected.value = date.toISOString().split("T")[0];
   updateChartForDate(date);
 });
+
+function highlightIfToday(date) {
+  const input = document.getElementById("selectedDate");
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const selected = new Date(date);
+  selected.setHours(0, 0, 0, 0);
+
+  if (selected.getTime() === today.getTime()) {
+    input.classList.add("today");
+  } else {
+    input.classList.remove("today");
+  }
+}
+
+function updateForwardButtonState(date) {
+  const forwardButton = document.getElementById("nextDate");
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const selected = new Date(date);
+  selected.setHours(0, 0, 0, 0);
+
+  forwardButton.disabled = selected.getTime() === today.getTime();
+}
